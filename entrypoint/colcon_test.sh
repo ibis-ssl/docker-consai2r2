edf7e81216e43d7a1b831b6d7ab203a70e937458
Mirai Hattori <mirai2045@gmail.com>
2019-12-23 23:31:54 +0900

    add colcon build --cmake-clean-cache when colcon_test

--- entrypoint/colcon_test.sh
+++ entrypoint/colcon_test.sh
@@ -3,4 +3,4 @@ source /opt/ros/dashing/setup.bash
 
 rosdep install --from-paths /userdir/colcon_ws/src --ignore-src -r -y
 
-cd /userdir/colcon_ws/ && colcon test && colcon test-result --verbose
+cd /userdir/colcon_ws/ && colcon build --cmake-clean-cache && colcon test && colcon test-result --verbose
